c0870916fdb0eea942bd19dc496e11d00f662f86
SkyLite / SkyNet <hkim6000@gmail.com>
2026-03-07 14:17:57 -0500

    Delete scripts directory

--- scripts/XysReport.js
+++
@@ -1,7 +0,0 @@
-﻿function CallReport(t) {
-    var data = [
-        { key: 'rpt', vlu: t.value }
-    ];
-    $WaitOn();
-    $ApiRequest(this, JSON.stringify(data));
-}
--- scripts/XysSent.js
+++
@@ -1,4 +0,0 @@
-﻿function NavXysSignin() {
-    $WaitOn();
-    $ApiRequest();
-} 
